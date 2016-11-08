03171a175fc75a306b97b1fdeb81a1d1a09647a4
Valery Barmin <valerybarmin@gmail.com>
2016-11-08 18:00:52 +0300

    lesson 1 added

--- lesson 1.R
+++ lesson 1.R
@@ -1,8 +1,23 @@
-rm(list = ls()) #очистить workspace
-cat("\014") #очистить консоль
-
-x <- c('10', '23', 'ten')
+x <- 1
+x <- c(10, 23, 'ten')
 x <- as.numeric(x)
-x[is.na(x)] <- 0
-x[x < 1] <- 1
 
+for(i in 1:length(x)) {
+  if(is.na(x[i])) {
+    x[i] <- 0
+  }
+  if(x[i] > 15 & x[i] < 25) {
+    x[i] <- x[i] + 2
+  }
+  if(x[i] == 10 | x[i] == 1) {
+    x[i] <- x[i] * 2
+  }
+}
+
+x[is.na(x)] <- 0
+x[x > 15 & x < 25] <- x[x > 15 & x < 25] + 2
+
+# hometask:
+#   изучить функции rep, length, seq,
+#   if / else (больше, меньше, равно, не равно),
+#   switch / case
