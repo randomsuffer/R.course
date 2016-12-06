17e97cf2cb2fd6cc0d38cb7746c2033910cb26c2
Valery Barmin <valerybarmin@gmail.com>
2016-12-06 15:34:17 +0300

    lesson 4 hometask solved

--- lesson 4.R
+++ lesson 4.R
@@ -20,10 +20,26 @@ df2 <- as.data.frame(matrix(UKgas, ncol = 4))
 colnames(df2) <- c('Qtr1', 'Qtr2', 'Qtr3', 'Qtr4')
 rownames(df2) <- c(1960:1986)
 
-barplot(df2, main = "Car Distribution by Gears and VS",
-        xlab = "Number of Gears",
-        # col = c('lightblue', 'mistyrose', 'lightcyan', 'lavender'),
-        legend = rownames(df2))
-
 # hometask
-# plot axis labels, pie chart size, stacked bar chart
+# plot y axis labels, pie chart size, stacked bar chart of df2
+
+barplot(df, main="1/4 mile time car distribution", xlab="Number of cars",
+        ylim = c(0,max(df)), axes = F)
+axis(2,seq(0,max(df),1),seq(0,max(df),1))
+
+pie(WorldPhones["1951",], labels = lbls, main = "Pie Chart of Countries", radius = 2)
+
+df2 <- t(df2)
+library(RColorBrewer)
+clrs <- brewer.pal(4, "GnBu")
+barplot(df2,
+        col = clrs,
+        main = "Quarterly UK gas consumption",
+        xlab = "Years",
+        ylab = "Millions of therms",
+        xlim = c(0, ncol(df2) * 1.5),
+        width = 1)
+legend("bottomright", 
+       legend = rownames(df2),
+       fill = clrs,
+       title = "Legend")
